50b83315d853138f5b7aacd0e2d7e8a4e7043d01
kikiwwwjin <36657109+kikiwwwjin@users.noreply.github.com>
2021-07-15 17:05:12 +0900

    7/15일 교육자료 수정

--- education/범주화_및_더미변수화.py
+++ education/범주화_및_더미변수화.py
@@ -53,7 +53,7 @@ dummies_df = pd.DataFrame(ohe_matrix, columns=encoder.classes_)
 df = pd.read_csv('C:\\Users\\wai\\Desktop\\프로젝트\\교육자료\\교육데이터\\BookExample.csv')
 
 # 실습 1) 'v1' 변수 범주화 하기
-v1_all_diff = df['v1'].max() - df['v1'].min() # v1 변수의 최대값 - 최소값(전체 구간)
+v1_all_diff = df.iloc[:,2].max() - df.iloc[:,2].min() # v1 변수의 최대값 - 최소값(전체 구간)
 # v1 변수는 고객의 신용도를 나타내는 변수 입니다.
 # v1 변수(신용도)를 신용등급으로 범주화 하는 작업을 해보겠습니다. (등급은 10개 등급으로 나눔)
 # v1 전체구간 10등분하기(1개 구간 차이)
@@ -71,27 +71,27 @@ print(labels) # 구간 개수에 따라 등급을 라벨링
 
 # 데이터 프레임에 'v1_grade' 신용 등급(10개의 구간을 일정한 구간의 크기로 구분함)
 # 1등급 : 최소값 <= x <= 1.276, 2등급 : 1.276 < x <= 2.552 .....
-df['v1_grade'] = pd.cut(df['v1'], bins=10, labels=labels)
+df['v1_grade'] = pd.cut(df.iloc[:,2], bins=10, labels=labels)
 print(df['v1_grade'] )
 
 # 데이터 프레임에 'v1_grade_quantile' 신용 등급(10개의 구간을 일정한 분위수로 구분함)
 # 변수의 변량에 따라서 분위수를 나누는 구간이 중복가능하기 때문에 duplicates 옵션에 'drop'을 해준다.
 # labels=False 옵션을 주어 낮은 구간부터 높은 구간순으로 0,1..6  +1씩 값이 증가하는 구간으로 세팅된다.
-df['v1_grade_quantile'] = pd.qcut(df['v1'], q=10, duplicates='drop', labels=False) # 7개의 구간으로 나누어졌다.
+df['v1_grade_quantile'] = pd.qcut(df.iloc[:,2], q=10, duplicates='drop', labels=False) # 7개의 구간으로 나누어졌다.
 # 0 등급부터 라벨링이 되기때문에 'v1_grade_quantile' 컬럼에 전체 적으로 +1을 해준다.
 df['v1_grade_quantile'] = df['v1_grade_quantile'] + 1
 print(df['v1_grade_quantile'])
 
 ### 실습 2) 'v31'변수(범주형 변수) 더미 변수화 하기
-print(df['v31'].unique()) # unique 확인 ['A' 'B' '0' 'C']
+print(df.iloc[:,-1].unique()) # unique 확인 ['A' 'B' '0' 'C']
 # 범주형 데이터는 모델의 학습 변수로 사용할 수 없다. 그래서 더미 변수화를 해주여야 사용할 수 있게 된다.
-dummies_df = pd.get_dummies(df['v31']) # 더미 변수를 생성
+dummies_df = pd.get_dummies(df.iloc[:,-1]) # 더미 변수를 생성
 result_df = pd.concat([df, dummies_df], axis=1) # 생성된 더미 변수를 원 데이터 뒤에 붙여준다.
 print(result_df) # 원 데이터 뒤에 프레임 뒤에 더미 변수가 생성된 것을 확인
 
 ### 실습 3) sklearn의 LabelEncoder, OneHotEncoder 함수로 더미 변수화 하기
 le = LabelEncoder()
-labels = le.fit_transform(df['v31']) # 데이터 변량에 따른 라벨링
+labels = le.fit_transform(df.iloc[:,-1]) # 데이터 변량에 따른 라벨링
 lb_matrix = labels.reshape(-1, 1) # 2차원 행렬로 변환
 print(lb_matrix.shape)
 
--- education/스케일링.py
+++ education/스케일링.py
@@ -1,14 +1,46 @@
+########################################################################################################################
+###################################################### python 스케일링 ##################################################
+########################################################################################################################
+
+
 import pandas as pd
 import numpy as np
 
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler
+import matplotlib.pyplot as plt
+
+# 0. scaling 할 numpy 선언
+scale_data = np.array([-150,-110,-30,0,20,30,110,120,130,150])
+
+
+# 1. StandardScaler : 평균을 0, 분산을 1로 변경됩니다.(이상치에 영향이 큽니다.)
 standardscaler = StandardScaler()
+print(scale_data, ',', standardscaler)
+std_scaling = standardscaler.fit_transform(scale_data.reshape(-1,1))
+print(std_scaling)
+# plt.hist(std_scaling, bins=std_scaling.shape[0])
+# plt.show()
 
-scale_data = np.array([0, 150, 130, 120 ,110])
-scale_data.reshape(-1, 1)
-print(standardscaler.fit_transform(scale_data.reshape(-1, 1)))
-
-
-scale_data
+# 2. MinmaxScaler : 모든 feature가 0과 1 사이에 위치하게 만듭니다.(이상치가 있을경우 다른 값들이 매우 압축된 범위에서만 움직이게 됩니다.)
 minmaxscaler = MinMaxScaler()
-minmaxscaler.fit_transform(scale_data.reshape(-1, 1))
+mm_scaling = minmaxscaler.fit_transform(scale_data.reshape(-1,1))
+print(mm_scaling)
+
+
+# 3. MaxAbsScaler : 절대값이 0~1사이에 매핑되도록 합니다. 즉 -1~1 사이로 재조정됩니다.
+# 양수 데이터로만 구성된 특징 데이터 셋에서는 MinMaxScaler와 유사하게 동작하며, 큰 이상치에 민감할 수 있습니다.
+maxabsscaler = MaxAbsScaler()
+maxabs_scaling = maxabsscaler.fit_transform(scale_data.reshape(-1,1))
+print(maxabs_scaling)
+
+# 4. RobustScaler : 모든 특성들이 같은 크기를 갖는다는 점에서 StandardScaler와 비슷하지만, 평균과 분산 대신 median과 quantile을 사용합니다.
+# 아웃라이어(이상치)에 영향을 최소화한 기법입니다. 중앙값(median)과 IQR(Interquantile range)을 사용하기 때문에 StandardScaler와 비교해보면
+# IQR = Q3 - Q1 : 즉, 25퍼센타일과 75퍼센타일의 값들을 다룬다.
+# 표준화 후 동일한 값을 더 넓게 분포시키고 있음을 확인 할 수 있습니다.
+robustscaler = RobustScaler()
+rb_scaling = robustscaler.fit_transform(scale_data.reshape(-1,1))
+print(rb_scaling)
+# plt.hist(rb_scaling, bins=rb_scaling.shape[0])
+
+### 실습 ###
+
